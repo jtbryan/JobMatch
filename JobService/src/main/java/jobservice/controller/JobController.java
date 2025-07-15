034bee9170b353aecd4a5b68f1ef4a86d6f847c3
Jonathan <jtbryan24@gmail.com>
2025-07-15 04:14:57 -0400

    Added delete.

--- JobService/src/main/java/jobservice/controller/JobController.java
+++ JobService/src/main/java/jobservice/controller/JobController.java
@@ -1,8 +1,9 @@
 package jobservice.controller;
 
 import jobservice.model.Job;
-import jobservice.repository.JobRepository;
+import jobservice.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,24 +14,30 @@ import java.util.List;
 public class JobController {
 
     @Autowired
-    private JobRepository jobRepository;
+    private JobService jobService;
 
     @PostMapping("/createJob")
     public ResponseEntity<Job> createJob(@RequestBody Job job) {
-        Job savedJob = jobRepository.save(job);
+        Job savedJob = jobService.createJob(job);
         return ResponseEntity.ok(savedJob);
     }
 
     @GetMapping("/jobs")
     public ResponseEntity<List<Job>> getJobs() {
-        List<Job> jobs = jobRepository.findAll();
+        List<Job> jobs = jobService.getAllJobs();
         return ResponseEntity.ok(jobs);
     }
 
     @GetMapping("/jobs/{id}")
     public ResponseEntity<Job> getJobsById(@PathVariable Long id) {
-        return jobRepository.findById(id)
+        return jobService.getJobById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/jobs/{id}")
+    public ResponseEntity deletedJobById(@PathVariable Long id) {
+        jobService.removeJobById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- JobService/src/main/java/jobservice/service/JobService.java
+++ JobService/src/main/java/jobservice/service/JobService.java
@@ -1,4 +1,32 @@
 package jobservice.service;
 
+import jobservice.model.Job;
+import jobservice.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class JobService {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public Job createJob(Job job) {
+        return jobRepository.save(job);
+    }
+
+    public List<Job> getAllJobs() {
+        return jobRepository.findAll();
+    }
+
+    public Optional<Job> getJobById(Long id) {
+        return jobRepository.findById(id);
+    }
+
+    public void removeJobById(Long id) {
+        jobRepository.deleteById(id);
+    }
 }
